2468717336d2b1e3843d959789603db922797faf
David <daveski084@users.noreply.github.com>
2020-10-19 22:01:30 -0400

    Initial commit

---
+++ Video Making Series - Copy - Copy/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float speed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movmentVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movmentVector *= speed;
+        rb.velocity = movmentVector;
+    }
+}
---
+++ Video Making Series - Copy - Copy/Assets/SavePlayerPosition.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.SceneManagement;
+
+public class SavePlayerPosition : MonoBehaviour
+{
+    public float x,
+                 y,
+                 z;
+
+    public void Save()
+    {
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
+
+        PlayerPrefs.SetFloat("x", x);
+        PlayerPrefs.SetFloat("y", y);
+        PlayerPrefs.SetFloat("z", z);
+    }
+
+    public void Load()
+    {
+        x = PlayerPrefs.GetFloat("x");
+        y = PlayerPrefs.GetFloat("y");
+        z = PlayerPrefs.GetFloat("z");
+
+        Vector3 playerLoadLocation = new Vector3(x, y, z);
+        transform.position = playerLoadLocation;
+    }
+}
